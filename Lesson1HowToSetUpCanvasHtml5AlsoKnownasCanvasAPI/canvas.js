3d64e7ca8f277530c6cfb7184cfc1232ad20a6d4
ArifinDepayJr <121757305+Arifindepayjr1@users.noreply.github.com>
2025-03-15 15:46:30 +0700

    Create canvas.js

---
+++ Lesson1HowToSetUpCanvasHtml5AlsoKnownasCanvasAPI/canvas.js
@@ -0,0 +1,8 @@
+const canvas  = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//context.fillRect(100 , 100 , 50 , 20);
+context.fillRect(20,20,50,50);
